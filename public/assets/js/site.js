8fc93f1ab6956c0d6a15441bc6148c87453f04e3
Hendra Susanto <benhanks040888@gmail.com>
2015-09-14 00:32:19 +0700

    client page

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -84,6 +84,45 @@ $(function() {
     fade: true
   });
 
+  $('.js-clients-slider-alt').slick({
+    dots: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    rows: 2,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]  });
+
   $('.js-clients-list').slick({
     dots: false,
     slidesToShow: 5,
